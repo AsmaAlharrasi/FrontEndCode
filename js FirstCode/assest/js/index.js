cf56f05fde9acca7792157233f3cf96beffdd666
AsmaAlharrasi <asmaalharrasi18@gmail.com>
2023-12-26 02:47:28 +0400

    updated2

---
+++ js FirstCode/assest/js/index.js
@@ -0,0 +1,10 @@
+let h = "Hello World!";
+let j = "From JavaScript";
+let obj = {
+    Name : "Asma",
+    Age : 22,
+    Address : "AlRustaq"
+};
+
+console.log(h + j + "\nMy name is " + obj.Name + " my age is " + obj.Age + " I live in " + obj.Address );
+console.log( `${h} ${j}`);
